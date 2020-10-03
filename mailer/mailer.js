bbfe885a6fd90139c334e75dbc8a1b9b77d76744
mauri1996 <mauri1996@hotmail.es>
2020-10-02 19:15:49 -0500

    sendgrid y variables de entorno

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,42 @@
-const nodemailer = require('nodemailer');
+// sirve para pruebas no en produccionse se isntalara sedgrid apra produccion
+//npm install nodemailer-sendgrid-transport
 // https://ethereal.email/create pagina donde se crea
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'helga81@ethereal.email',
-        pass: 'sk7qR9GZqMRehGTYvv'
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+let mailConfig;
+
+if (process.env.NODE_ENV === 'production') {
+    const options ={
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
+    };
+    mailConfig = sgTransport(options);
+
+} else {
+    
+    if (process.env.NODE_ENV === 'staging') {
+        console.log('XXXXXXXXXXXXXXXXXXX');
+        const options ={
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        };
+        mailConfig = sgTransport(options);
+    }else{
+        
+        const options = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+                
+            }
+        };
+        mailConfig = options;
     }
-};
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
